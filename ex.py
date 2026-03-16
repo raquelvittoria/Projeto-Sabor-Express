5faf813ac1f551ea4f92cfe7b71b00f80fd71b4a
raquelvittoria <raquelvittoriaalves@gmail.com>
2026-03-16 20:25:28 -0300

    Removendo ex.py e ex2.py do versionamento

--- ex.py
+++
@@ -1,5 +0,0 @@
-numero = int(input('Escolha um número: '))
-if numero % 2 == 0:
-    print(f"O numero {numero} é par")
-else:
-    print(f"O numero {numero} é impar")
--- ex2.py
+++
@@ -1,8 +0,0 @@
-idade = int(input('Qual é a sua idade?'))
-if idade <= 12:
-    print('Criança')
-elif 13 <= idade <= 18:
-    print('Adolescente')
-else:
-    print('Adulto')
-    
